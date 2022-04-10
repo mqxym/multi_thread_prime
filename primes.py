a5411f42c3be68bd40b77967626a3696c2058b49
mQxYm <maxim@omg.lol>
2022-04-11 01:06:04 +0200

    parallel calculation

--- primes.py
+++ primes.py
@@ -1,27 +1,26 @@
 import sys
+from threading import Thread
 
-lower = 1234567
-upper = 1235000
+lower = 1
+upper = 400
 
-threads = 4
+threads_count = 7
 
-if lower > upper:
-    sys.exit("lower number is higher than upper number")
-
-if threads > 64 or threads < 1:
-    sys.exit("Thread counter wrong")
-
-if (lower < 0):
-    sys.exit("Number can't be lover than 0")
+threads = []
 
 
-def main():
-    print("Prime numbers between", lower, "and", upper, "are:")
-    checkPrimes(lower, upper)
-    
+def testInput ():
+    if lower > upper:
+        sys.exit("lower number is higher than upper number")
 
+    if threads_count > 64 or threads_count < 1:
+        sys.exit("Thread counter wrong")
 
-def checkPrimes(lower, upper):
+    if (lower <= 0):
+        sys.exit("Number can't be lover than 0")
+
+def checkPrimes(lower, upper, threadID):
+    print("Thread from ", lower, " to ", upper," ThreadID: ", threadID, "\n")
     for num in range(lower, upper + 1):
         # all prime numbers are greater than 1
         if num > 1:
@@ -29,7 +28,30 @@ def checkPrimes(lower, upper):
                 if (num % i) == 0:
                     break
             else:
-                print(num)
+                print("Prime: ",num)
+
+    #print("Thread ",threadID ," finished.", )
+
+def main():
+    testInput()
+    difference = round((upper - lower)/threads_count)
+
+    print("Prime numbers between", lower, "and", upper, "are:")
+
+    for i in range(0, threads_count+1): 
+        threadLow = lower + (difference*i)
+        threadUp = lower + (difference * (i+1))
+        print("Up, ", threadUp, " Low ", threadLow )
+        thread = Thread(target=checkPrimes, args=(threadLow,threadUp,i,))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("Calculatin finished")
 
 
 if __name__ == "__main__":
