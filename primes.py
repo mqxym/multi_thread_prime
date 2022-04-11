6b3b086038277abe0b478f6fe4f6dc83879bfe5e
mQxYm <maxim@omg.lol>
2022-04-11 18:52:43 +0200

    sorting at the end

--- primes.py
+++ primes.py
@@ -1,16 +1,18 @@
 import sys
+import copy
 from threading import Thread
 
 #Idea: write output to file
 #Idea create a satus 
 
 
-lower = 5000000
-upper = 6000000
+lower = 1
+upper = 600
 
 threadCount = 7
 
 threads = []
+results = []
 
 
 def testInput ():
@@ -23,6 +25,35 @@ def testInput ():
     if (lower <= 0):
         sys.exit("Number can't be lover than 0")
 
+def swap(arr, pos1, pos2):
+    tmp = arr[pos1]
+    arr[pos1] = arr[pos2]
+    arr[pos2] = tmp
+    return arr
+ 
+def quicksort(arr):
+    p = arr[len(arr)-1] # p ist letzes Element im Array
+    z = 0 #Zähler für Position im Array
+    for i in range(len(arr)):
+        # print(arr[i], p, z)
+        if arr[i] <= p:
+            arr = swap(arr, i, z)
+            z = z+1     
+    
+    arr1 = arr[0:z-1] # Erster Teil des Arrays ohne p
+    arr2 = arr[z:len(arr)] #Zweiter Teil des Arrays ohne p
+    if len(arr1) > 1:
+        arr1 = quicksort(arr1)
+    if len(arr2) > 1:
+        arr2 = quicksort(arr2)
+        
+    # Sortiertes Array zusammensetzen aus arr1, p, arr2
+    arr0 = copy.deepcopy(arr1)
+    arr0.append(p)
+    arr0.extend(arr2)
+    return(arr0)
+    
+
 def checkPrimes(lower, upper, threadID):
     print("Thread start ", lower, " to ", upper," ThreadID: ", threadID, "\n")
     for num in range(lower, upper + 1):  
@@ -32,7 +63,8 @@ def checkPrimes(lower, upper, threadID):
                 if (num % i) == 0:
                     break
             else:
-                print("Prime: ",num)
+                #print("Prime: ",num)
+                results.append(num)
 
     #print("Thread ",threadID ," finished.", )
 
@@ -55,8 +87,8 @@ def main():
     for thread in threads:
         thread.join()
 
-    print("Calculatin finished")
-
+    print("Calculating finished")
+    print(quicksort(results))
 
 if __name__ == "__main__":
     main()
