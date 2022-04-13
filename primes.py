e880d61028d36cf94b29bf8ad08128625cf119a9
mQxYm <maxim@omg.lol>
2022-04-13 19:34:07 +0200

    improved library import

--- primes.py
+++ primes.py
@@ -1,16 +1,14 @@
-from concurrent.futures import thread
 import sys
 from threading import Thread
 import lib 
 import hashlib
 
-
 lower = 0
 upper = 777
 limit = 1000 #How many primes you want to calculate
 
-data = "" #Change Data when you want primes based on data
-computingPower = 6 #Every increase is increased by *16
+data = "North Korea" #Change Data when you want primes based on data
+computingPower = 7 #Every increase is increased by *16
 
 if (len(sys.argv)==2):
     data = sys.argv[1]
